db3528694d8860a4991e1fc8071e702e5ce8649c
Iamscottm8 <39254773+Iamscottm8@users.noreply.github.com>
2018-05-14 15:48:20 +1000

    Update Form1.Designer.cs

--- Eve V Calibration Backup/Form1.Designer.cs
+++ Eve V Calibration Backup/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Eve_V_Calibration_Backup
+namespace Eve_V_Calibration_Backup
 {
     partial class Form1
     {
@@ -30,7 +30,6 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -54,14 +53,6 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
-            this.label1.TabIndex = 2;
-            // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(53, 41);
@@ -76,9 +67,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(181, 92);
+            this.ClientSize = new System.Drawing.Size(181, 73);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -87,7 +77,6 @@
             this.Name = "Form1";
             this.Text = "Eve V CalMAN Backup";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,7 +84,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
     }
 }
